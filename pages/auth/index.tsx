39d40fc10029a11aec9f0858fa20ef16b010dd5b
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-18 12:57:49 +0100

    feat: create email input field and set value

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,6 +1,8 @@
 import Input from '@/components/Input/Input';
+import { ChangeEvent, useState } from 'react';
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
   return (
     <div className='relative h-full w-full bg-[url("/images/hero.jpg")] bg-no-repeat bg-center bg-fixed bg-cover'>
       <div className='bg-black w-full h-full lg:bg-opacity-50'>
@@ -11,7 +13,15 @@ const Auth = () => {
           <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>Sign in</h2>
             <div className='flex flex-col gap-4'>
-              <Input />
+              <Input
+                id='email'
+                label='Email'
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setEmail(event.target.value)
+                }
+                type='email'
+                value={email}
+              />
             </div>
           </div>
         </div>
