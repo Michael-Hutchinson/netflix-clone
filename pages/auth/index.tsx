c9b39c7ae027db975d00bdad0f4b3455abc15e02
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-18 13:04:30 +0100

    feat: create login button

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -43,6 +43,9 @@ const Auth = () => {
                 value={password}
               />
             </div>
+            <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+              Login
+            </button>
           </div>
         </div>
       </div>
