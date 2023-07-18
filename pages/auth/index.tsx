cfa1ed992fb72617b9765a390e4f8dd7b626ffa9
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-18 12:58:23 +0100

    feat: create username input field

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent, useState } from 'react';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   return (
     <div className='relative h-full w-full bg-[url("/images/hero.jpg")] bg-no-repeat bg-center bg-fixed bg-cover'>
       <div className='bg-black w-full h-full lg:bg-opacity-50'>
@@ -13,6 +14,15 @@ const Auth = () => {
           <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>Sign in</h2>
             <div className='flex flex-col gap-4'>
+              <Input
+                id='username'
+                label='Username'
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setUsername(event.target.value)
+                }
+                type='username'
+                value={username}
+              />
               <Input
                 id='email'
                 label='Email'
