9454a188953ab39bb67a9aab4257287e8d34625a
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-18 13:12:47 +0100

    feat: only show username on register form

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -20,18 +20,20 @@ const Auth = () => {
         <div className='flex justify-center'>
           <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>
-              {variant === 'login' ? 'Sign in' : 'Create an account'}
+              {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
             <div className='flex flex-col gap-4'>
-              <Input
-                id='username'
-                label='Username'
-                onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                  setUsername(event.target.value)
-                }
-                type='text'
-                value={username}
-              />
+              {variant === 'register' && (
+                <Input
+                  id='username'
+                  label='Username'
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    setUsername(event.target.value)
+                  }
+                  type='text'
+                  value={username}
+                />
+              )}
               <Input
                 id='email'
                 label='Email'
