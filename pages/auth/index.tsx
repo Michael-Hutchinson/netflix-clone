9102e186df89a9a0a00526ccfc39686bfae971cd
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-25 19:49:41 +0100

    feat: add a sign in with github onclick

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -98,7 +98,14 @@ const Auth = () => {
               <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
                 <FcGoogle size={24} />
               </div>
-              <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+              <div
+                onClick={() =>
+                  signIn('github', {
+                    callbackUrl: '/',
+                  })
+                }
+                className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'
+              >
                 <FaGithub size={24} />
               </div>
             </div>
