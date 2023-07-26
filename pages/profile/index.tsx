0d5733e10ddc3cf5d99a5bbc49866dbf3562bb3c
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-07-26 12:06:53 +0100

    feat: create styling for profile page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
@@ -17,9 +18,26 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profile = () => {
+  const { data: user } = useCurrentUser();
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className='flex items-center h-full justify-center'>
+      <div className='flex flex-col'>
+        <h1 className='text-3xl md:text-6xl text-white text-center'>
+          Who is watching?
+        </h1>
+        <div className='flex items-center justify-center gap-8 mt-10'>
+          <div onClick={() => {}}>
+            <div className='group flex-row w-44 mx-auto'>
+              <div className='w-44 h-44 border-2 rounded-md flex items-center justify-center border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden'>
+                <img src='/images/default-blue.png' alt='profile image' />
+              </div>
+              <div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white group-hover:cursor-pointer'>
+                {user?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
