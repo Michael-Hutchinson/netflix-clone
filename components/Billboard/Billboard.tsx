ec8757eb3fe3b65c8f0eb8d3da8fad4c87b3a01d
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-10-13 14:15:35 +0100

    feat: add description for billboard

--- components/Billboard/Billboard.tsx
+++ components/Billboard/Billboard.tsx
@@ -16,6 +16,9 @@ const Billboard = () => {
         <p className='text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl'>
           {data?.title}
         </p>
+        <p className='text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl'>
+          {data?.description}
+        </p>
       </div>
     </div>
   );
