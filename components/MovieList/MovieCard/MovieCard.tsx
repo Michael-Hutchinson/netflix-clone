80fba88aa1424883b9959ee7bc8cad6bb1155a7e
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-08 18:39:33 +0000

    feat: add button to open modal on movie list

--- components/MovieList/MovieCard/MovieCard.tsx
+++ components/MovieList/MovieCard/MovieCard.tsx
@@ -1,7 +1,9 @@
 import Favourite from '@/components/Button/Favourite/Favourite';
+import useModal from '@/hooks/useModal';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { BsFillPlayFill } from 'react-icons/bs';
+import { BiChevronDown } from 'react-icons/bi';
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -9,6 +11,8 @@ interface MovieCardProps {
 
 const MovieCard = ({ data }: MovieCardProps) => {
   const router = useRouter();
+  const { openModal } = useModal();
+
   return (
     <div className='group bg-zinc-900 col-span relative h-[12vw]'>
       <img
@@ -31,6 +35,15 @@ const MovieCard = ({ data }: MovieCardProps) => {
               <BsFillPlayFill size={30} />
             </button>
             <Favourite movieId={data?.id} />
+            <button
+              onClick={() => openModal(data?.id)}
+              className='cursor-pointer ml-auto group.item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300'
+            >
+              <BiChevronDown
+                className='text-white group-hover/item:text-neutral-300'
+                size={30}
+              />
+            </button>
           </div>
           <p className='text-green-400 font-semibold mt-4'>
             New <span className='text-white'>2003</span>
