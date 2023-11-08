c5d41a7f8cd07582b9c13c50bc670fb0ecc3273d
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-08 16:38:16 +0000

    feat: create a favourite button

--- components/Button/Favourite/Favourite.tsx
+++ components/Button/Favourite/Favourite.tsx
@@ -2,16 +2,54 @@ import axios from 'axios';
 import React, { useCallback, useMemo } from 'react';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useFavourites from '@/hooks/useFavourites';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlineCheck, AiOutlinePlus } from 'react-icons/ai';
 
 interface FavouriteProps {
   movieId: string;
 }
 
 const Favourite = ({ movieId }: FavouriteProps) => {
+  const { mutate: mutateFavourites } = useFavourites();
+  const { data: currentUser, mutate } = useCurrentUser();
+
+  const isFavourite = useMemo(() => {
+    const list = currentUser?.favouriteIds || [];
+
+    return list.includes(movieId);
+  }, [currentUser?.favouriteIds, movieId]);
+
+  const toggleFavourites = useCallback(async () => {
+    let response;
+
+    if (isFavourite) {
+      response = await axios.delete(`/api/favourite/${movieId}`);
+    } else {
+      response = await axios.post('/api/favourite', { movieId });
+    }
+
+    const updatedFavouriteIds = response?.data?.favouriteIds;
+
+    mutate({
+      ...currentUser,
+      favouriteIds: updatedFavouriteIds,
+    });
+
+    mutateFavourites();
+  }, [currentUser, isFavourite, movieId, mutate, mutateFavourites]);
+
+  const Icon = isFavourite ? AiOutlineCheck : AiOutlinePlus;
+
   return (
-    <div className='cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-netural-300'>
-      <AiOutlinePlus className='text-white' size={25} />
+    <div
+      onClick={toggleFavourites}
+      onKeyDown={(event) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+          toggleFavourites();
+        }
+      }}
+      className='cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-netural-300'
+    >
+      <Icon className='text-white' size={25} />
     </div>
   );
 };
