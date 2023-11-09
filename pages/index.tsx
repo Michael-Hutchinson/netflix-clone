21dffe46c057928dabd864696a3f8c2ff94b5d56
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-09 20:21:51 +0000

    fix: add error handling to see why session isnt working

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,21 @@ import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      },
-    };
+  try {
+    const session = await getSession(context);
+    if (!session) {
+      return {
+        redirect: {
+          destination: '/auth',
+          permanent: false,
+        },
+      };
+    }
+    return { props: {} };
+  } catch (error) {
+    console.error('Error getting session:', error);
+    return { props: {} };
   }
-  return { props: {} };
 }
 
 export default function Home() {
